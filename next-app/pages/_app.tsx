446c7687e809c3db0ac4c74176c48b649a76bccf
HabacucMX <habacuc.vera@gmail.com>
2023-01-23 13:48:18 -0600

    RPC Chain set to Hyperspace testnet

--- next-app/pages/_app.tsx
+++ next-app/pages/_app.tsx
@@ -2,29 +2,47 @@ import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
 import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit';
-import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
+import {configureChains, createClient, WagmiConfig } from 'wagmi';
 import { publicProvider } from 'wagmi/providers/public';
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+
+
+import { Chain } from '@wagmi/core'
+ 
+export const filecoin: Chain = {
+  id: 3141,
+  name: 'Filecoin - Hyperspace testnet',
+  network: 'filecoin',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Filecon',
+    symbol: 'FIL',
+  },
+  rpcUrls: {
+    default: { http: ['https://api.hyperspace.node.glif.io/rpc/v0'] },
+  },
+  blockExplorers: {
+    etherscan: { name: 'Filfox', url: 'https://hyperspace.filfox.info/en' },
+    default: { name: 'FilFox', url: 'https://hyperspace.filfox.info/en' },
+  },
+  //contracts: {
+  //  multicall3: {
+  //    address: '0xca11bde05977b3631167028862be2a173976ca11',
+  //    blockCreated: 11907934,
+  //  },
+  //},
+}
 
 const { chains, provider } = configureChains(
+  [filecoin], //  <<<<<< Gnosis Chain is now the only option any other network will say "wrong network"
   [
-    {
-      id: 3141,
-      name: "Filecoin - Hyperspace testnet",
-      network: "filecoin",
-      testnet: true,
-      rpcUrls: {
-        default: "https://api.hyperspace.node.glif.io/rpc/v0", 
-      },
-    },
-  ],
-  [
+    jsonRpcProvider({ rpc: () => ({ http: "https://api.hyperspace.node.glif.io/rpc/v0" }) }),  //<<<< New RPC Provider
     publicProvider(),
+    // No need to alchemy
   ]
 );
 
-
-const { connectors } = getDefaultWallets({
+ const { connectors } = getDefaultWallets({
   appName: 'RainbowKit App',
   chains,
 });
