cbdf12c80a0398993452abcd1e190a52717a48d9
HabacucMX <habacuc.vera@gmail.com>
2023-01-23 11:46:36 -0600

    next js app with rainbow kit

---
+++ pages/_app.tsx
@@ -0,0 +1,50 @@
+import '../styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import type { AppProps } from 'next/app';
+import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [
+    {
+      id: 3141,
+      name: "Filecoin - Hyperspace testnet",
+      network: "filecoin",
+      testnet: true,
+      rpcUrls: {
+        default: "https://api.hyperspace.node.glif.io/rpc/v0", 
+      },
+    },
+    
+  ],
+  [
+    alchemyProvider({ apiKey: "UP7-vCgH4OfFwUygRDLg8dXpADY-zb3T" }),
+    publicProvider(),
+  ]
+);
+
+
+const { connectors } = getDefaultWallets({
+  appName: 'RainbowKit App',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+export default MyApp;
