a61eedb35304beb20ff07d136b4f97b8fbd85507
HabacucMX <habacuc.vera@gmail.com>
2023-01-26 15:37:57 -0600

    daoProposals base grid

---
+++ next-app/pages/daoProposals.tsx
@@ -0,0 +1,13 @@
+export default function daoProposals () {
+    return(
+        <div className='from-cata-300 to-mods-300 bg-gradient-to-br h-screen grid items-center'>
+            <div className="card0">
+                <div className="propDashboard">
+                    <div className="propCard">hello1</div>
+                    <div className="propCard">hello2</div>
+                </div>
+                <div className="daoMembers">hello3</div>
+            </div>
+        </div>
+    )
+}
