7d7e0dc8ac15b9b6dc924c656ca86c5fdcc73fd1
HabacucMX <habacuc.vera@gmail.com>
2023-01-28 11:23:27 -0600

    Update on mini proposal layout

--- next-app/pages/daoProposals.tsx
+++ next-app/pages/daoProposals.tsx
@@ -7,20 +7,53 @@ export default function daoProposals () {
                 <div className="propDashboard">
                     <div className="propCard relative">
                         <div className="bg-white rounded-t-gen grid grid-cols-6">
-                            <div className="col-span-4 p-5">
-                                <div className="font-bold text-3xl">Proposal Tittle</div>
-                                <div className="font-bold text-xl">Type of activity</div>
-                                <div className="font-bold text-xl">01/01/2023</div>
-                                <div className="italic text-xl py-3">City, Latitude, Longitude</div>
+                            <div className="col-span-4 p-5 my-auto">
+                                <div className="font-bold text-xl">#1 Proposal Tittle</div>
+                                <div className="font-bold">Type of activity</div>
+                                <div className="font-bold">01/01/2023</div>
                             </div>
-                            <div className="col-span-2 border-l-2 border-b-2 border-gray-500/50 rounded-tr-gen rounded-bl-gen grid grid-rows-6">
-                                <div className="row-span-2 text-center grid items-center font-bold text-xl rounded-tr-gen rounded-bl-gen border-b-2 border-gray-500/50">On review</div>
-                                <div className="row-span-4 text-center grid items-center">Evidence states</div>
+                            <div className="
+                                col-span-2                                 
+                                border-gray-500/50 
+                                rounded-tr-gen 
+                                rounded-bl-gen grid grid-rows-3"
+                            >
+                                <div className="
+                                    grid 
+                                    items-center text-center 
+                                    font-bold text-xl 
+                                    rounded-tr-gen rounded-bl-gen 
+                                    bg-cata-300"
+                                >
+                                    On review
+                                </div>
+                                <div className="text-center grid grid-cols-5 items-center gap-3 mx-auto p-3 w-full">
+                                    <div className="mx-auto">
+                                        <div className="h-3 w-3 rounded-full bg-cata-500"></div>     
+                                    </div>
+                                    <div className="mx-auto">
+                                        <div className="h-3 w-3 rounded-full bg-cata-500"></div>     
+                                    </div>
+                                    <div className="mx-auto">
+                                        <div className="h-3 w-3 rounded-full bg-cata-500"></div>     
+                                    </div>
+                                    <div className="mx-auto">
+                                        <div className="h-3 w-3 rounded-full bg-cata-500"></div>     
+                                    </div>
+                                    <div className="mx-auto">
+                                        <div className="h-3 w-3 rounded-full bg-cata-500"></div>     
+                                    </div>
+                                </div>
+                                <div className="bg-mods-300/50 hover:bg-mods-300 cursor-pointer hover:text-white grid items-center text-xl px-10 rounded-tl-gen"> 
+                                    Details
+                                </div>
                             </div>
                         </div>
-                        <div className="bg-gray-200/50 rounded-b-gen">
+                        <div className="bg-gray-200/50 rounded-b-gen p-5 relative">
+                            <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-full text-center italic bg-mods-900/50 text-white p-3 rounded-b-gen">City, Latitude, Longitude</div>
+                            
                         </div>
-                        <div className="absolute bottom-0 right-0 bg-mods-300/50 hover:bg-mods-300 cursor-pointer hover:text-white py-3 text-xl px-10 rounded-br-gen rounded-tl-gen"> Details</div>
+               
                     </div>
                     <Link href='/'>
                         <div className="newProp ">+ Add new proposal</div>
