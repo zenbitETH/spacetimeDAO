1c55b945028fc7480cf56d0fdd2241c1119b3668
HabacucMX <habacuc.vera@gmail.com>
2023-01-28 13:36:10 -0600

    Form step 1

--- next-app/components/ProgressBar.tsx
+++ next-app/components/ProgressBar.tsx
@@ -1,41 +1,55 @@
 export default function ProgressBar () {
     return(              
-        <div className="formProgress">
-            <div className="flex ">
-                <div className="flex-1">
-                    <div className="progressCircles bg-gray-300" />
-                </div>
-                <div className="progressBar">
-                    <div className="barContainer">
-                        <div className="barBar bg-gray-300"></div>
+        <div>
+            <div className="formProgress">
+                <div className="flex ">
+                    <div className="flex-1">
+                        <div className="progressCircles bg-gray-300" />
+                    </div>
+                    <div className="progressBar">
+                        <div className="barContainer">
+                            <div className="barBar bg-gray-300"></div>
+                        </div>
+                    </div>
+                    <div className="flex-1">
+                        <div className="progressCircles bg-gray-300" />
+                    </div>
+                    <div className="progressBar ">
+                        <div className="barContainer">
+                            <div className="barBar bg-gray-300" ></div>
+                        </div>
+                    </div>
+                    <div className="flex-1">
+                        <div className="progressCircles bg-gray-300" />
+                    </div>
+                    <div className="progressBar">
+                        <div className="barContainer">
+                            <div className="barBar bg-gray-300"></div>
+                        </div>
+                    </div>
+                    <div className="flex-1">
+                        <div className="progressCircles bg-gray-300" />
                     </div>
                 </div>
-                <div className="flex-1">
-                    <div className="progressCircles bg-gray-300" />
+            </div>
+            <div className="
+                grid mx-auto w-2/3 px-6 grid-cols-4 
+                text-center text-sm"
+            >
+                <div className="font-bold underline underline-offset-4">
+                    General
                 </div>
-                <div className="progressBar ">
-                    <div className="barContainer">
-                        <div className="barBar bg-gray-300" ></div>
-                    </div>
+
+                <div className="">
+                    Location
                 </div>
-                <div className="flex-1">
-                    <div className="progressCircles bg-gray-300" />
+
+                <div className="">
+                    Evidence
                 </div>
-                <div className="progressBar">
-                    <div className="barContainer">
-                        <div className="barBar bg-gray-300"></div>
-                    </div>
-                </div>
-                <div className="flex-1">
-                    <div className="progressCircles bg-gray-300" />
-                </div>
-                <div className="progressBar">
-                    <div className="barContainer">
-                        <div className="barBar bg-gray-300"></div>
-                    </div>
-                </div>
-                <div className="flex-1">
-                    <div className="progressCircles bg-gray-300" />
+
+                <div className="">
+                    Confirm
                 </div>
             </div>
         </div>
--- next-app/pages/newProposal.tsx
+++ next-app/pages/newProposal.tsx
@@ -8,6 +8,36 @@ export default function newProposal () {
             h-screen grid items-center text-center mx-auto">
             <div className="formCard">
                 <ProgressBar/>
+                <div className='p-5 gap-5 grid'>
+                    <div>
+                        <div className='formLabel'>Proposal Tittle</div>
+
+                        <input type='text' placeholder='Name your proposal' className='placeholder:italic px-6 py-3 text-black w-full border-color1-500 mx-auto rounded-dd'></input>
+
+                    </div>
+                    <div>
+                        <div className='formLabel'>Type of activity</div>
+                        <select className='pl-5 text-black w-full rounded-dd px-6 py-3 border-color1-500'>
+                            <option value="" selected>Please select</option>
+                            <option>🗳️ Improvement in public administration</option>
+                            <option>🌳 Face-to-face event</option>
+                            <option>💻 Online event</option>
+                            <option>⚠️ Security Report</option>
+                            <option>👷 Request work or maintenance</option>
+                            <option>⚙️ Add function to the DAO</option>
+
+                        </select>
+                    </div>
+                    <div className='row-span-2 w-full mx-auto h-72'>
+                        <div className='formLabel'>Description</div>
+                        <textarea placeholder='Describe your proposal' className='p-5 text-black w-full mx-auto rounded-dd placeholder:italic h-full border-color1-500'></textarea>
+                    </div>
+                    <div className='text-center w-full my-auto pt-14 w-44 mx-auto'>
+                        <div className='px-10 py-3 hover bg-mods-100 rounded-gen cursor-pointer text-white'>
+                            Continue
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
