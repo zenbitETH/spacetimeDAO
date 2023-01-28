d4926582ad6e3067cf128e58ba5e70ef1765f48f
HabacucMX <habacuc.vera@gmail.com>
2023-01-28 12:39:45 -0600

    New Proposal Page

--- next-app/pages/newProposal.tsx
+++ next-app/pages/newProposal.tsx
@@ -1,7 +1,14 @@
+import ProgressBar from "../components/ProgressBar"
+
+
 export default function newProposal () {
     return(
-        <div className="from-cata-300 to-mods-300 bg-gradient-to-br h-screen grid items-center text-center">
-            <div>hello</div>
+        <div className="
+            from-cata-300 to-mods-300 bg-gradient-to-br 
+            h-screen grid items-center text-center mx-auto">
+            <div className="formCard">
+                <ProgressBar/>
+            </div>
         </div>
     )
 }
