24cc160c918ed5b2e822568ec5504d3f358ef202
HabacucMX <habacuc.vera@gmail.com>
2023-01-28 12:40:09 -0600

    Progress Bar

---
+++ next-app/components/ProgressBar.tsx
@@ -0,0 +1,43 @@
+export default function ProgressBar () {
+    return(              
+        <div className="formProgress">
+            <div className="flex ">
+                <div className="flex-1">
+                    <div className="progressCircles bg-gray-300" />
+                </div>
+                <div className="progressBar">
+                    <div className="barContainer">
+                        <div className="barBar bg-gray-300"></div>
+                    </div>
+                </div>
+                <div className="flex-1">
+                    <div className="progressCircles bg-gray-300" />
+                </div>
+                <div className="progressBar ">
+                    <div className="barContainer">
+                        <div className="barBar bg-gray-300" ></div>
+                    </div>
+                </div>
+                <div className="flex-1">
+                    <div className="progressCircles bg-gray-300" />
+                </div>
+                <div className="progressBar">
+                    <div className="barContainer">
+                        <div className="barBar bg-gray-300"></div>
+                    </div>
+                </div>
+                <div className="flex-1">
+                    <div className="progressCircles bg-gray-300" />
+                </div>
+                <div className="progressBar">
+                    <div className="barContainer">
+                        <div className="barBar bg-gray-300"></div>
+                    </div>
+                </div>
+                <div className="flex-1">
+                    <div className="progressCircles bg-gray-300" />
+                </div>
+            </div>
+        </div>
+    )
+}
