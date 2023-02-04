d22635c466c8cf7071df8d6dbaf89cec7ab04d27
HabacucMX <habacuc.vera@gmail.com>
2023-02-03 18:46:21 -0600

    Update on proposal states component

--- next-app/pages/proposal-id.tsx
+++ next-app/pages/proposal-id.tsx
@@ -53,13 +53,52 @@ export default function proposalDetail() {
                 </div>
                 <div className=" bg-white/50 rounded-dd row-span-6"> 
                     <div className=" grid grid-rows-2  p-3 gap-3 h-full">
-                        <div className="blockedEvidence ">Moderators will review your proposal</div>
+                        <div className="grid grid-cols-2 gap-3 items-center  bg-white rounded-dd relative">
+                            <div className="bg-white rounded-dd ">
+                                <div className="text-left font-bau p-3">
+
+                                    <div className="text-center">01/01/2023</div>
+                                    <div className="text-justify text-sm pt-3 font-exo">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="h-full bg-gray-300/60 rounded-dd grid items-center text-sm">
+                                Your evidence will be previewed here
+                            </div>
+                            <div className="absolute top-0 right-0 p-2 bg-gray-500 rounded-bl-dd rounded-tr-dd text-white text-sm">
+                                <div className="italic"> Local Response</div>
+                                <div className="text-[12px] -mb-1">made on 01/02/23</div>
+                                <div className="text-[12px]">made by 0x123..abc</div>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-2 gap-3 items-center  bg-white rounded-dd relative">
+                            <div className="bg-white rounded-dd ">
+                                <div className="text-left font-bau p-3">
+
+                                    <div className="text-center">01/01/2023</div>
+                                    <div className="text-justify text-sm pt-3 font-exo">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="h-full bg-gray-300/60 rounded-dd grid items-center text-sm">
+                                Your evidence will be previewed here
+                            </div>
+                            <div className="absolute top-0 right-0 p-2 bg-gray-500 rounded-bl-dd rounded-tr-dd text-white text-sm">
+                                <div className="italic"> Local Response</div>
+                                <div className="text-[12px] -mb-1">made on 01/02/23</div>
+                                <div className="text-[12px]">made by 0x123..abc</div>
+                            </div>
+                        </div>
+                        
+                        
                        
                         {/* NEW PROPOSAL / BLOCKED EVIDENCE
                             <div className="blockedEvidence ">Moderators will review your proposal</div>
 
                             ON REVIEW / LOCAL REQUEST                            
-                            <div className="newEvidence ">Upload local request</div>\
+                            <div className="newEvidence ">Upload local request</div>
                             
                             ON REVIEW / LOCAL RESPONSE
                             <div className="blockedEvidence2 ">Once you have the local response <br/> Please upload the file</div>
@@ -112,22 +151,71 @@ export default function proposalDetail() {
                     </div>
                 </div>
                 <div className="grid grid-rows-2 gap-3 row-span-6 ">
-                    <div className="stateCard row-span-2 ">
+
+                    <div className="stateCard  from-cata-300 to-mods-300 bg-gradient-to-r row-span-2">
                         <div className="mx-auto grid gap-3">
                             <div className=" text-white text-3xl">Proposal State</div>
-                            <div className="text-7xl">New Proposal</div>
-                            <div className="cardBT	"> ⚙️Change State</div>
+                            <div className="text-7xl">Voting</div>
+                            <div className="voteCard">
+                                <div className="italic col-span-2">0/10 citizen moderators have voted <br/>
+                                    2/10 government moderators have votes</div>
+                                <div className="cardBT"><span className="text-5xl">👍</span></div>
+                                <div className="cardBT"><span className="text-5xl">👎</span></div>
+                            </div>
                         </div>
                     </div>
+                        
+                    
                     
                     
                  
-                    {/* ON REVIEW STATE
+                    {/* 
+                        NEW PROPOSAL STATE
                         <div className="stateCard row-span-2 ">
                             <div className="mx-auto grid gap-3">
                                 <div className=" text-white text-3xl">Proposal State</div>
-                                <div className="text-7xl">On review</div>
-                                <div className="cardBT	"> ⚙️Change State</div>
+                                <div className="text-7xl">New Proposal</div>
+                                <div className="grid grid-cols-2">
+                                    <div className="cardBT	"> <span className="text-2xl">🔍 </span>Ask for local evidence</div>
+                                    <div className="cardBT	"> <span className="text-2xl">🗳️ </span>Ready for voting</div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        ON REVIEW STATE / WAITING FOR LOCAL EVIDENCE
+                       <div className="stateCard row-span-2 ">
+                            <div className="mx-auto grid gap-3">
+                                <div className=" text-white text-3xl">Proposal State</div>
+                                <div className="text-7xl">🔍On review
+                                    <div className="text-2xl italic">Waiting for local evidence</div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        ON REVIEW / LOCAL EVIDENCE ON REVIEW
+                        <div className="stateCard row-span-2 ">
+                            <div className="mx-auto grid gap-3">
+                                <div className=" text-white text-3xl">Proposal State</div>
+                                <div className="text-7xl">🔍On review
+                                    <div className="text-2xl italic">Local evidence on review</div>
+                                </div>
+
+                                <div className="grid grid-cols-2">
+                                    <div className="cardBT	"> <span className="text-2xl">✅ </span>Mark as solved</div>
+                                    <div className="cardBT	"> <span className="text-2xl">🗳️ </span>Ready for voting</div>
+                                </div>
+                            </div>
+                        </div>
+                    */}
+
+                    {/* SOLVED STATE
+                        <div className="stateCard row-span-2 ">
+                            <div className="mx-auto grid gap-3">
+                                <div className=" text-white text-3xl">Proposal State</div>
+                                <div className="text-7xl">✅Solved
+                                    <div className="text-2xl italic">Solved with local evidence</div>
+                                </div>
                             </div>
                         </div>
                     */}
@@ -137,16 +225,15 @@ export default function proposalDetail() {
                             <div className="mx-auto grid gap-3">
                                 <div className=" text-white text-3xl">Proposal State</div>
                                 <div className="text-7xl">Voting</div>
-
                                 <div className="voteCard">
                                     <div className="italic col-span-2">0/10 citizen moderators have voted <br/>
                                         2/10 government moderators have votes</div>
-                                    <div className="cardBT">👍</div>
-                                    <div className="cardBT">👎</div>
+                                    <div className="cardBT"><span className="text-5xl">👍</span></div>
+                                    <div className="cardBT"><span className="text-5xl">👎</span></div>
                                 </div>
                             </div>
-
                         </div>
+                        
                     */}
 
                 </div>
