e8d9d5a8f9f44aa29c9133f739841118ef1d4aeb
shyam-patel-kira <shyampkira@gmail.com>
2023-04-02 20:59:03 -0400

    Update packages

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,39 +1,32 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("hardhat-deploy")
 require("hardhat-deploy-ethers")
-// require("./tasks")
+require("./tasks")
 require("dotenv").config()
-// require('@nomiclabs/hardhat-waffle')
-
-// module.exports = {
-//   networks: {
-//     hardhat: {
-//       blockGasLimit: 8000000,
-//     },
-//     filecoin: {
-//       url: "http://127.0.0.1:1234",
-//       type: "filecoin"
-//     }
-//   },
-//   solc: {
-//     version: "0.8.0",
-//     optimizer: {
-//       enabled: true,
-//       runs: 200
-//     }
-//   }
-// };
-
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: "0.8.17",
-    defaultNetwork: "hyperspace",
+    solidity: {
+        version: "0.8.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+            details: { yul: false },
+          },
+        },
+      },
+    defaultNetwork: "Hyperspace",
     networks: {
-        hyperspace: {
+        Hyperspace: {
             chainId: 3141,
             url: "https://api.hyperspace.node.glif.io/rpc/v1",
+            accounts: [PRIVATE_KEY],
+        },
+        FilecoinMainnet: {
+            chainId: 314,
+            url: "https://api.node.glif.io",
             accounts: [PRIVATE_KEY],
         },
     },
